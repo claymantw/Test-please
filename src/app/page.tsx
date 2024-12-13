8ed947508b155db771194db918afd224207df1ba
Christian Montoya <christian.montoya@consensys.net>
2024-12-13 12:56:02 -0800

    Minesweeper attempt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const frame = {
     title: "Launch Frame",
     action: {
       type: "launch_frame",
-      name: "Farcaster Frames v2 Demo",
+      name: "Minesweeper",
       url: appUrl,
       splashImageUrl: `${appUrl}/splash.png`,
       splashBackgroundColor: "#f7f7f7",
@@ -22,10 +22,10 @@ export const revalidate = 300;
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "Farcaster Frames v2 Demo",
+    title: "Minesweeper",
     openGraph: {
-      title: "Farcaster Frames v2 Demo",
-      description: "A Farcaster Frames v2 demo app.",
+      title: "Minesweeper",
+      description: "The classic puzzle game in a Farcaster Frame.",
     },
     other: {
       "fc:frame": JSON.stringify(frame),
---
+++ src/components/Game.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import sdk, {
+  type FrameContext,
+} from "@farcaster/frame-sdk";
+/*
+import {
+  useAccount,
+  useSendTransaction,
+  useSignMessage,
+  useSignTypedData,
+  useWaitForTransactionReceipt,
+  useDisconnect,
+  useConnect,
+  useSwitchChain,
+  useChainId,
+} from "wagmi";
+
+import { config } from "~/components/providers/WagmiProvider";
+import { Button } from "~/components/ui/Button";
+import { truncateAddress } from "~/lib/truncateAddress";
+import { base, optimism } from "wagmi/chains";
+import { BaseError, UserRejectedRequestError } from "viem";
+*/
+
+export default function Game(
+  { title }: { title?: string } = { title: "Minesweeper" }
+) {
+  const appUrl = process.env.NEXT_PUBLIC_URL;
+
+  const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+  const [context, setContext] = useState<FrameContext>();
+
+  useEffect(() => {
+    const load = async () => {
+      setContext(await sdk.context);
+      sdk.actions.ready();
+    };
+    if (sdk && !isSDKLoaded) {
+      setIsSDKLoaded(true);
+      load();
+    }
+  }, [isSDKLoaded]);
+
+  if (!isSDKLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div id="game">Loading...</div>
+      <script type="text/javascript" src={`${appUrl}/minesweeper.js`}></script>
+      <script type="text/javascript">
+        startMinisweeper(document.getElementById('game')); 
+      </script>
+    </div>
+  );
+}
