f740d68f989c113dfa26e97e94b36432c363750a
Christian Montoya <christian.montoya@consensys.net>
2024-12-31 09:18:24 -0500

    Remove wagmi provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,12 +3,13 @@
 import dynamic from "next/dynamic";
 
 const WagmiProvider = dynamic(
-  () => import("~/components/providers/WagmiProvider"),
+  // () => import("~/components/providers/WagmiProvider"),
   {
     ssr: false,
   }
 );
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <WagmiProvider>{children}</WagmiProvider>;
+  // return <WagmiProvider>{children}</WagmiProvider>;
+  return <>{children}</>;
 }
