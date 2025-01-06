5b4f114d7bbd14c06589d5a31b5b0f3c65a6865e
Christian Montoya <christian.montoya@consensys.net>
2025-01-06 09:16:40 -0500

    The most shortest

--- public/minisweeper.js
+++ public/minisweeper.js
@@ -353,7 +353,7 @@ const shareStats = () => {
   if(!farcasterSDK || !appURL) return false; 
   // const gamesPlayedSuffix = minisweeperState.stats.games == 1 ? '' : 's'; 
   const gamesWonSuffix = minisweeperState.stats.wins == 1 ? '' : 's'; 
-  let shareText = `My 💣 Minesweeper stats: 🏆 ${minisweeperState.stats.wins} win${gamesWonSuffix}, 📊 ${(Math.round(minisweeperState.stats.wins *100) / minisweeperState.stats.games).toFixed(2)}% win rate.`; 
+  let shareText = `My Minesweeper stats: ${minisweeperState.stats.wins} win${gamesWonSuffix}, 📊 ${(Math.round(minisweeperState.stats.wins *100) / minisweeperState.stats.games).toFixed(2)}% win rate.`; 
   if(minisweeperState.stats.fastest) { 
     shareText += ` Fastest: ${parseInt(minisweeperState.stats.fastest/1000)}.${parseInt(minisweeperState.stats.fastest%1000)}s!`; 
   }
